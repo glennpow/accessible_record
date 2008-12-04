e4c1d34e70bdcbe6c1210ee7d0347c9f3e3702ba
glennpow <glennpow@gmail.com>
2008-12-03 20:44:51 -0800

    Fixed validations for nested attributes

--- lib/accessible_record.rb
+++ lib/accessible_record.rb
@@ -3,7 +3,7 @@ module AccessibleRecord
     base.extend(MacroMethods)              
   end
   
-  module MacroMethods          
+  module MacroMethods
     def has_accessible(*args)
       write_inheritable_attribute(:accessible_associations, args)
 
@@ -59,6 +59,35 @@ module AccessibleRecord
         end
       end
       super(attributes)
+    end
+    
+    def valid?
+      super
+      
+      self.class.read_inheritable_attribute(:accessible_associations).each do |attribute|
+        if association = self.class.reflect_on_association(attribute)
+          case association.macro
+          when :has_one, :belongs_to
+            if attr_object = self.send(attribute)
+              unless attr_object.valid?
+                attr_object.errors.each_full do |message|
+                  self.errors.add(attribute, message)
+                end
+              end
+            end
+          when :has_many
+            self.send(attribute).each do |attr_object|
+              unless attr_object.valid?
+                attr_object.errors.each_full do |message|
+                  self.errors.add(attribute, message)
+                end
+              end
+            end
+          end
+        end
+      end
+        
+      errors.empty?
     end
   end
 end
